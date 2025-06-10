1ab3dd190b08f38a62a646c7befcd4dbd1c7c5cc
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-10 18:36:43 +0200

    Fix pagination and add config in the settings.py

--- cinema/views.py
+++ cinema/views.py
@@ -120,16 +120,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         return MovieSessionSerializer
 
 
-class OrderSetPagination(PageNumberPagination):
-    page_size = 1
-    page_size_query_param = "page_size"
-    max_page_size = 10
-
-
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    pagination_class = OrderSetPagination
 
     def get_queryset(self):
         queryset = self.queryset.filter(user=self.request.user)
